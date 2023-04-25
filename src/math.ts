eecb9d66fdc3979073a53e1a619276f35d9c9d57
Shivam Mishra <scm.mymail@gmail.com>
2023-04-25 08:34:53 +0530

    feat: add tests for math

--- src/math.ts
+++ src/math.ts
@@ -1,18 +1,17 @@
 export function weightedAverage(values: number[], weights?: number[]): number {
-  const weightsToTest = weights ?? [];
-  if (weightsToTest.length && values.length !== weightsToTest.length) {
+  if (weights && values.length !== weights.length) {
     throw new Error(
       "The number of values must be equal to the number of weights"
     );
   }
 
   const sum = values.reduce((prev, curr, index) => {
-    const weight = weightsToTest.length ? weightsToTest[index] : 1;
+    const weight = weights ? weights[index] : 1;
     return prev + curr * weight;
   }, 0);
 
-  const totalWeight = weightsToTest
-    ? weightsToTest.reduce((prev, curr) => prev + curr, 0)
+  const totalWeight = weights
+    ? weights.reduce((prev, curr) => prev + curr, 0)
     : values.length;
 
   return sum / totalWeight;
---
+++ test/math.spec.ts
@@ -0,0 +1,28 @@
+import { expect, describe, it } from "vitest";
+import { weightedAverage } from "../src/math";
+
+describe("weightedAverage", () => {
+  it("calculates the correct weighted average with default weights", () => {
+    const values = [1, 2, 3, 4, 5];
+    const expected = (1 + 2 + 3 + 4 + 5) / 5;
+    const result = weightedAverage(values);
+    expect(result).toEqual(expected);
+  });
+
+  it("calculates the correct weighted average with custom weights", () => {
+    const values = [1, 2, 3, 4, 5];
+    const weights = [1, 2, 3, 4, 5];
+    const expected =
+      (1 * 1 + 2 * 2 + 3 * 3 + 4 * 4 + 5 * 5) / (1 + 2 + 3 + 4 + 5);
+    const result = weightedAverage(values, weights);
+    expect(result).toEqual(expected);
+  });
+
+  it("throws an error if the number of values and weights are different", () => {
+    const values = [1, 2, 3];
+    const weights = [1, 2, 3, 4, 5];
+    expect(() => weightedAverage(values, weights)).toThrow(
+      "The number of values must be equal to the number of weights"
+    );
+  });
+});
