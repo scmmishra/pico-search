74fa7ce3e963f7c3de587e842eb6d3bc99b43d04
Shivam Mishra <scm.mymail@gmail.com>
2023-04-27 14:03:05 +0530

    feat: update levenshtein

--- src/algorithms/levenshtein.ts
+++ src/algorithms/levenshtein.ts
@@ -1,56 +1,35 @@
-/**
- * Calculates the Damereau-Levenshtein similarity between two strings.
- * @param str1 The first string.
- * @param str2 The second string.
- * @returns The similarity between the two strings as a number between 0 and 1.
- */
-export default function levenshtein(str1: string, str2: string): number {
-  str1 = str1.trim().toLowerCase();
-  str2 = str2.trim().toLowerCase();
-  // Step 1: Initialize the matrix with the Levenshtein distance between each prefix of str1 and each prefix of str2.
-  const m = str1.length;
-  const n = str2.length;
-  const d: number[][] = [];
+export default function levenshtein(word1: string, word2: string): number {
+  const distance = levenshteinDistance(word1, word2);
+  const maxLength = Math.max(word1.length, word2.length);
+  return 1 - distance / maxLength;
+}
 
-  // Initialize the first column of the matrix to the Levenshtein distances between each prefix of str1 and an empty string.
+function levenshteinDistance(word1: string, word2: string): number {
+  const m = word1.length;
+  const n = word2.length;
+  const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+
   for (let i = 0; i <= m; i++) {
-    d[i] = [];
-    d[i][0] = i;
+    dp[i][0] = i;
   }
 
-  // Initialize the first row of the matrix to the Levenshtein distances between an empty string and each prefix of str2.
-  for (let j = 1; j <= n; j++) {
-    d[0][j] = j;
+  for (let j = 0; j <= n; j++) {
+    dp[0][j] = j;
   }
 
-  // Step 2: Compute the Levenshtein distance between each prefix of str1 and each prefix of str2 using dynamic programming.
   for (let i = 1; i <= m; i++) {
     for (let j = 1; j <= n; j++) {
-      // Calculate the cost of the substitution operation.
-      const cost = str1[i - 1] === str2[j - 1] ? 0 : 1;
-
-      // Compute the minimum of the three possible operations: deletion, insertion, and substitution.
-      d[i][j] = Math.min(
-        d[i - 1][j] + 1, // deletion
-        d[i][j - 1] + 1, // insertion
-        d[i - 1][j - 1] + cost // substitution
-      );
-
-      // Check if a transposition operation is possible and if it results in a smaller distance.
-      if (
-        i > 1 &&
-        j > 1 &&
-        str1[i - 1] === str2[j - 2] &&
-        str1[i - 2] === str2[j - 1]
-      ) {
-        d[i][j] = Math.min(d[i][j], d[i - 2][j - 2] + cost); // transposition
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.min(
+          dp[i - 1][j - 1] + 1,
+          dp[i][j - 1] + 1,
+          dp[i - 1][j] + 1
+        );
       }
     }
   }
 
-  // Step 3: Calculate the similarity between the two strings as 1 minus the normalized Levenshtein distance.
-  const distance = d[m][n];
-  const maxLength = Math.max(m, n);
-  const similarity = 1 - distance / maxLength;
-  return similarity;
+  return dp[m][n];
 }
--- test/alogrithms/levenshtein.spec.ts
+++ test/alogrithms/levenshtein.spec.ts
@@ -1,28 +1,53 @@
 import { describe, expect, it } from "vitest";
 import levenshtein from "../../src/algorithms/levenshtein";
 
-describe("levenshtein", () => {
-  it("should return 1 for identical strings", () => {
-    expect(levenshtein("hello", "hello")).toBe(1);
-  });
+const pairs: Array<[string, string, number]> = [
+  ["computer", "cucumber", 0.375], // words with similar characters but different length
+  ["hello", "world", 0.19999999999999996], // words with no common characters
+  ["elephant", "relevant", 0.625], // words with common characters but different order
+  ["bake", "cake", 0.75], // short words with one letter difference
+  ["test", "rest", 0.75], // short words with one letter difference
+  ["backpack", "knapsack", 0.375], // longer words with similar characters and same length
+  ["education", "adoption", 0.5555555555555556], // words with no common characters
+  ["world", "words", 0.6], // words with similar characters but different length
+  ["faith", "fate", 0.6], // short words with similar characters but different length
+  ["measure", "treasure", 0.75], // longer words with similar characters and same length
+  ["music", "magic", 0.6], // short words with similar characters and same length
+  ["information", "reformation", 0.8181818181818181], // longer words with similar characters but different length
+  ["expect", "accept", 0.33333333333333337], // short words with similar characters but different length
+  ["ceremony", "ceramics", 0.5], // words with similar characters but different length and different order
+  ["consult", "insult", 0.7142857142857143], // short words with similar characters and same length but different order
+  ["keyboard", "keychain", 0.5], // words with similar characters but different length and different order
+  ["toddler", "older", 0.5714285714285714], // words with no common characters and different length
+  ["republic", "public", 0.75], // longer words with one letter difference
+  ["decision", "precision", 0.7777777777777778], // longer words with similar characters and same length but different order
 
-  it("should return 0 for completely different strings", () => {
-    expect(levenshtein("aaaaa", "zzzzz")).toBe(0);
-  });
+  // extreme cases
+  ["apple", "", 0], // an empty string compared to a non-empty string
+  ["💻🔋🚀", "🚀🔋💻", 0.6666666666666667], // strings containing emojis that look similar but in different orders
+  ["racecar", "abcdefghijklmnopqrstuvwxyz", 0.11538461538461542], // a string with all distinct characters compared to a palindrome
+  ["one", "two", 0], // words with no common characters
+  [
+    "thisisaverylongwordthatdoesnotmatchtheotherword",
+    "word",
+    0.08510638297872342,
+  ], // a very long string compared to a short string
+  ["abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz", 1], // a long string compared to itself
+  ["abcde", "vwxyz", 0], // words with no common characters
+  ["red", "blue", 0], // words with no common characters
+  ["aaaaaab", "aaaaaac", 0.857], // strings with long common prefix but different last character
+  [
+    "a",
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    0.015,
+  ], // a very long string compared to a single character
+  ["b", "a", 0], // short words with no common characters
+];
 
-  it("should return the correct similarity for slightly different strings", () => {
-    expect(levenshtein("kitten", "sitting")).toBeCloseTo(0.57, 2);
-  });
-
-  it("should return the correct similarity for case-insensitive strings", () => {
-    expect(levenshtein("Hello", "hELLo")).toBe(1);
-  });
-
-  it("should return the correct similarity for strings with leading/trailing whitespace", () => {
-    expect(levenshtein("   hello", "hello   ")).toBe(1);
-  });
-
-  it("should return the correct similarity for strings with special characters", () => {
-    expect(levenshtein("café", "coffee")).toBeCloseTo(0.3, 0.6);
+describe("levenshetin", () => {
+  it("should return correct value based on pairs", () => {
+    pairs.forEach(([str1, str2, expected]) => {
+      expect(levenshtein(str1, str2)).toBeCloseTo(expected, 2);
+    });
   });
 });
