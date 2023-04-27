95e26e00335d88720fae1f46d2b13f3a6cb9f51d
Shivam Mishra <scm.mymail@gmail.com>
2023-04-27 12:33:59 +0530

    chore: update jaro winkler

--- src/algorithms/jaroWinkler.ts
+++ src/algorithms/jaroWinkler.ts
@@ -5,65 +5,72 @@
  * @returns A floating-point number between 0 and 1, where 0 means the strings are completely dissimilar and 1 means they are identical.
  */
 export default function jaroWinkler(str1: string, str2: string): number {
+  // Compute Jaro-Winkler distance between two string
+  // Swap strings if str1 is shorter than string 2
   str1 = str1.trim().toLowerCase();
   str2 = str2.trim().toLowerCase();
 
-  // Jaro distance
-  const m: boolean[] = []; // match flags
+  if (str1.length < str2.length) {
+    const tempString: string = str1;
+    str1 = str2;
+    str2 = tempString;
+  }
+
+  let len1: number = str1.length;
+  let len2: number = str2.length;
+  if (!len2) {
+    return 0.0;
+  }
+
+  const delta: number = Math.max(1, len1 / 2.0) - 1.0;
+
+  // Flags for transpositions
+  let flag: boolean[] = Array(len2).fill(false);
+  let ch1Match: string[] = Array(len1).fill("");
+  // Count number of matching characters
   let matches = 0;
-  let transpositions = 0;
-  let aMatch = "";
-  let bMatch = "";
-
-  const len1 = str1.length;
-  const len2 = str2.length;
-  const maxDistance = Math.floor(Math.max(len1, len2) / 2) - 1;
-
-  // Calculate matches and transpositions for Jaro distance
-  for (let i = 0; i < len1; i++) {
-    const start = Math.max(0, i - maxDistance);
-    const end = Math.min(i + maxDistance + 1, len2);
-    for (let j = start; j < end; j++) {
-      if (m[j] || str1[i] !== str2[j]) {
-        continue;
+  // Check if characters on both string matches
+  for (let i: number = 0; i < len1; i++) {
+    const ch1: string = str1[i];
+    for (let j = 0; j < len2; j++) {
+      const ch2: string = str2[j];
+      if (j <= i + delta && j + delta >= 1 && ch1 == ch2 && !flag[j]) {
+        flag[j] = true;
+        ch1Match[matches++] = ch1;
+        break;
       }
-      m[j] = true;
-      matches++;
-      if (i === j) {
-        // Exact match
-        aMatch += str1[i];
-      } else {
-        // Partial match
-        aMatch += str1[i];
-        bMatch += str2[j];
+    }
+  }
+  if (!matches) {
+    return 1.0;
+  }
+  // Count number of transpositions (shared characters placed in different positions)
+  let transpositions: number = 0.0;
+  for (let i: number = 0, j: number = 0; j < len2; j++) {
+    if (flag[j]) {
+      if (str2[j] != ch1Match[i]) {
         transpositions++;
       }
-      break;
+      i++;
     }
   }
 
-  // Check for zero matches
-  if (matches === 0) {
-    return 0;
+  const m: number = matches;
+
+  // Jaro Similarity Formula simj = ( (m / length of s1) + (m / length of s2) + (m - t) / m ) / 3
+  const jaro: number =
+    (m / len1 + m / len2 + (m - transpositions / 2.0) / m) / 3.0;
+
+  // Length of common prefix between string up to 4 characters
+  let commonPrefix: number = 0.0;
+  len2 = Math.min(4, len2);
+
+  for (let i: number = 0; i < len2; i++) {
+    if (str1[i] == str2[i]) {
+      commonPrefix++;
+    }
   }
 
-  // Calculate Jaro distance
-  const jaroDistance =
-    (matches / len1 +
-      matches / len2 +
-      (matches - transpositions / 2) / matches) /
-    3;
-
-  // Jaro-Winkler distance
-  let prefix = 0;
-
-  // Calculate prefix match for Jaro-Winkler distance
-  while (str1[prefix] === str2[prefix] && prefix < 4) {
-    prefix++;
-  }
-
-  // Calculate Jaro-Winkler distance
-  const jaroWinklerDistance = jaroDistance + prefix * 0.1 * (1 - jaroDistance);
-
-  return jaroWinklerDistance;
+  // Jaro Winkler Distance Formula simw = simj + lp(1 - simj)
+  return 1.0 - (jaro + commonPrefix * 0.1 * (1.0 - jaro));
 }
