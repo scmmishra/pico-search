004424a3ed198196b37a16e22d5d86c2cdb2b31e
Shivam Mishra <scm.mymail@gmail.com>
2023-06-03 10:57:24 +0530

    feat: add test for clamp

--- test/math.spec.ts
+++ test/math.spec.ts
@@ -1,5 +1,5 @@
 import { expect, describe, it } from "vitest";
-import { weightedAverage } from "../src/math";
+import { weightedAverage, clamp } from "../src/math";
 
 describe("weightedAverage", () => {
   it("calculates the correct weighted average with default weights", () => {
@@ -24,5 +24,25 @@ describe("weightedAverage", () => {
     expect(() => weightedAverage(values, weights)).toThrow(
       "The number of values must be equal to the number of weights"
     );
+  });
+});
+
+describe("clamp", () => {
+  it("returns the value if it is within the range", () => {
+    expect(clamp(0.5)).toBe(0.5);
+    expect(clamp(0.5, 0, 1)).toBe(0.5);
+    expect(clamp(0.5, -1, 1)).toBe(0.5);
+  });
+
+  it("returns the minimum value if the value is less than the minimum", () => {
+    expect(clamp(-1)).toBe(0);
+    expect(clamp(-1, 0, 1)).toBe(0);
+    expect(clamp(-1, -2, 1)).toBe(-1);
+  });
+
+  it("returns the maximum value if the value is greater than the maximum", () => {
+    expect(clamp(2)).toBe(1);
+    expect(clamp(2, 0, 1)).toBe(1);
+    expect(clamp(2, -1, 2)).toBe(2);
   });
 });
