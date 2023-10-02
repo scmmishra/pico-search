f2db1b963f44c902ba95f08023a2b893043a9145
Shivam Mishra <scm.mymail@gmail.com>
2023-10-02 12:22:11 +0530

    feat: remove banner

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "tsup";
-import packageJson from "./package.json";
 
 export default defineConfig((options) => {
   return {
@@ -11,12 +10,5 @@ export default defineConfig((options) => {
     dts: true,
     sourcemap: true,
     clean: true,
-    esbuildOptions(options) {
-      options.banner = {
-        js: `// Pico Search v${
-          packageJson.version
-        } · The MIT License · Copyright © ${new Date().getFullYear()} Shivam Mishra.`,
-      };
-    },
   };
 });
